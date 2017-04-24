aadbf0272346d84941510c3128d2717feebde8e3
pbout <pavlos.bountagkidis@outlook.com>
2017-04-24 20:19:23 +0100

    Added coursework files

---
+++ src/gui/Constants.java
@@ -0,0 +1,13 @@
+package gui;
+
+public interface Constants {
+  double GAME_WIDTH_PROP = 0.78;       //Width of the game portion (prop of total)
+  double GAME_HEIGHT_PROP = 1.0;      //Height of the game portion (prop of total)
+
+  //Dimensions of the error pane (in pixels)
+  int ERROR_WIDTH = 500;
+  int ERROR_HEIGHT = 150;
+  double INFO_SIZE = 0.5;    //How much of the screen should the info make up?
+
+  String ROOT = "/Users/keith/Courses/PiJ/coursework/cw-temple/code/src/images/";
+}
