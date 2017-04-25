08ac6204d739f84bbfb51b658492eefe1a467b78
pbout <pavlos.bountagkidis@outlook.com>
2017-04-25 03:08:24 +0100

    not much progress after reaching orb...

--- src/student/Explorer.java
+++ src/student/Explorer.java
@@ -3,6 +3,9 @@ package student;
 import game.EscapeState;
 import game.ExplorationState;
 import game.NodeStatus;
+import game.Node;
+import game.Tile;
+
 
 
 import java.util.*;
@@ -78,7 +81,6 @@ public class Explorer {
    * @param state the information available at the current state
    */
   public void escape(EscapeState state) {
-    //TODO: Escape from the cavern before time runs out
   }
 
   /**
