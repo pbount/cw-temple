13fcc0b54fd96c42440663f822e1de64c48e1712
pbout <pavlos.bountagkidis@outlook.com>
2017-05-09 23:17:17 +0100

    Working on first escape. No progress

--- src/student/Explorer.java
+++ src/student/Explorer.java
@@ -18,6 +18,10 @@ public class Explorer {
   // a breadcrum for every block
   private long[] breadcrum = new long[2];
 
+  // exit strategy
+  ArrayList<ArrayList<Node>> steps = new ArrayList<>();
+  Set<Node> scanned = new HashSet<>();
+
 
 
   /**
@@ -51,6 +55,7 @@ public class Explorer {
    * @param state the information available at the current state
    */
   public void explore(ExplorationState state) {
+    breadcrumSetter(state.getCurrentLocation());
     while(state.getDistanceToTarget() > 0){
       state.moveTo(closestNeighbourToOrb(state.getNeighbours(), state));
       breadcrumSetter(state.getCurrentLocation());
@@ -81,7 +86,92 @@ public class Explorer {
    * @param state the information available at the current state
    */
   public void escape(EscapeState state) {
+
+    Set<Node> currentNodes = new HashSet<>();
+    ArrayList<Node> step = new ArrayList<>();
+    currentNodes = state.getCurrentNode().getNeighbours();
+    scanned.addAll(currentNodes);
+    step.addAll(currentNodes);
+    steps.add(step);
+    do{
+      Set<Node> temp = getLastStepElements();
+      //System.out.println("The size of temp is: " + temp.size());
+      step.clear();
+      step = getUniqueNeighbourList(getLastStepElements());
+      steps.add(step);
+      scanned.addAll(step);
+      System.out.println("size of last: " + getLastStepElements().size());
+      System.out.println("scannedsize: " + scanned.size());
+    }while(!scanned.contains(state.getExit()));
+
+
+
+
+
+
+
+
+    /**
+    ArrayList<Node> neighbours = new ArrayList<>();
+    ArrayList<Node> layer = new ArrayList<>();
+    Set<Node> currentNodes = new HashSet<>();
+
+    currentNodes = getNodeNeighbours(state.getCurrentNode());
+    layer.addAll(currentNodes);
+    layers.add(layer);
+
+    while (!layers.contains(state.getExit())){
+      ArrayList<Node> newLayer = new ArrayList<>();
+      for (Node n : layer){
+        newLayer.addAll(getUniqueNeighbours(getNodeNeighbours(n)));
+      }
+      layers.add(newLayer);
+      layer = newLayer;
+      newLayer.clear();
+      System.out.println("the size of the final is: " + layers.size());
+    }
+
+    System.out.println("The last block is found. Distance: " + layers.size());
+    */
+
+
+
+    /**
+    boolean exitfound = false;
+    Node exitNode = state.getExit();
+    ArrayList<ArrayList<Node>> levels = new ArrayList<>();
+    ArrayList<Node> neighbours = new ArrayList<>();
+    neighbours.addAll(state.getCurrentNode().getNeighbours());
+    levels.add(neighbours);
+    int step = 0;
+
+    while (!exitfound){
+      ArrayList<Node> temp = new ArrayList<>();
+      ArrayList<Node> toAdd = new ArrayList<>();
+      neighbours = null;
+
+      for (Node n : temp){
+        toAdd.addAll(state.getCurrentNode().getNeighbours());
+        for (Node x : toAdd){
+          if (!levels.contains(x)){
+            neighbours.add(x);
+          }
+        }
+
+        if (neighbours.contains(exitNode)){
+          exitfound = true;
+        }
+      }
+
+    }
+
+    System.out.println("steps so far: "+ step);
+
+     */
   }
+
+
+
 
   /**
    *
@@ -92,7 +182,6 @@ public class Explorer {
   public long closestNeighbourToOrb(Collection<NodeStatus> neighbours, ExplorationState state){
     long result = neighbours.iterator().next().getId();
     for (NodeStatus neighbour : neighbours){
-      System.out.println("the current neighbour is:" + neighbour.getId() + " and it's link is: " + neighbour + "and it was visited:" + breadcrumCounter(neighbour.getId()));
       if (neighbour.getDistanceToTarget() < state.getDistanceToTarget() && breadcrumCounter(neighbour.getId())==0){
         result = neighbour.getId();
       }else if(breadcrumCounter(result) > breadcrumCounter(neighbour.getId())){
@@ -135,5 +224,37 @@ public class Explorer {
     return result;
   }
 
+  public Set<Node> getUniqueNeighbours(Set<Node> nodeCollection){
+    Set<Node> uniqueNodeCollection = new HashSet<>();
+    uniqueNodeCollection.clear();
+
+    for (Node n : nodeCollection){
+      for (Node nj : n.getNeighbours()){
+        if (!scanned.contains(nj)){
+          uniqueNodeCollection.add(nj);
+          scanned.add(nj);
+        }
+      }
+    }
+
+    return uniqueNodeCollection;
+  }
+
+  public ArrayList<Node> getUniqueNeighbourList(Set<Node> nodeCollection){
+        ArrayList<Node> uniqueNodeCollection = new ArrayList<>();
+        uniqueNodeCollection.addAll(getUniqueNeighbours(nodeCollection));
+        return uniqueNodeCollection;
+  }
+
+  public ArrayList<Node> getLastStep(){
+    ArrayList<Node> result = steps.get(steps.size() - 1);
+    return result;
+  }
+
+  public Set<Node> getLastStepElements(){
+      Set<Node> result = new HashSet<>();
+      result.addAll(getLastStep());
+      return result;
+  }
 
 }
